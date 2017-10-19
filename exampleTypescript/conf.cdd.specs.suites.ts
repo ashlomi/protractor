97062548f5ec10e4efe4e0580b7f8360db7c7c17
cde-test-user <cde.test.user@gmail.com>
2017-10-19 16:03:00 +0300

    Update conf.cdd.specs.suites.ts

--- exampleTypescript/conf.cdd.specs.suites.ts
+++ exampleTypescript/conf.cdd.specs.suites.ts
@@ -10,19 +10,19 @@ export let config: Config = {
   
 // Specs taken from CDD protractor tests
 // not to be executed here
-specs: [
-        '/releases/01.1.releasesPage.spec.ts',
-	'/releases/01.2.createRelease.spec.ts',
-        '/releases/01.3.releaseActions.spec.ts',
-	'/users/*.spec.ts',
-	'/tracks/26.1.tracksPage.spec.ts',
-	'/tracks/26.2.trackActions.spec.ts',
-	'/tracks/26.3.trackIndications.spec.ts',
-	'/tracks/26.4.releaseInTrack.spec.ts',
-	'/tracks/26.5.createTrack.spec.ts',
-	'/tracks/26.6.trackPage.spec.ts',
-	'/tracks/26.7.approveReleaseInTrack.spec.ts'
-        ],
+// specs: [
+//         '/releases/01.1.releasesPage.spec.ts',
+// 	'/releases/01.2.createRelease.spec.ts',
+//         '/releases/01.3.releaseActions.spec.ts',
+// 	'/users/*.spec.ts',
+// 	'/tracks/26.1.tracksPage.spec.ts',
+// 	'/tracks/26.2.trackActions.spec.ts',
+// 	'/tracks/26.3.trackIndications.spec.ts',
+// 	'/tracks/26.4.releaseInTrack.spec.ts',
+// 	'/tracks/26.5.createTrack.spec.ts',
+// 	'/tracks/26.6.trackPage.spec.ts',
+// 	'/tracks/26.7.approveReleaseInTrack.spec.ts'
+//         ],
 	
 suites: {
 	"00_logIn": ['/login/login.spec.ts'],
