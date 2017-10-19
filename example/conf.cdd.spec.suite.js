d07e4776425ffa292f6435abc0f9912e05783f30
cde-test-user <cde.test.user@gmail.com>
2017-10-19 16:59:06 +0300

    CDD TAR conf js specs + suites file

--- example/conf.cdd.spec.suite.js
+++ example/conf.cdd.spec.suite.js
@@ -12,18 +12,18 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: [
-        '/releases/01.1.releasesPage.spec.js',
-        '/releases/01.2.createRelease.spec.js',
-        '/releases/01.3.releaseActions.spec.js',
-        '/users/*.spec.js',
-        '/tracks/26.1.tracksPage.spec.js',
-        '/tracks/26.2.trackActions.spec.js',
-        '/tracks/26.3.trackIndications.spec.js',
-        '/tracks/26.4.releaseInTrack.spec.js',
-        '/tracks/26.5.createTrack.spec.js',
-        '/tracks/26.6.trackPage.spec.js',
-        '/tracks/26.7.approveReleaseInTrack.spec.js'
+specs: [
+        '/spec/releases/01.1.releasesPage.spec.js',
+	'/spec/releases/01.2.createRelease.spec.js',
+        '/spec/releases/01.3.releaseActions.spec.js',
+	'/spec/users/*.spec.js',
+	'/spec/tracks/26.1.tracksPage.spec.js',
+	'/spec/tracks/26.2.trackActions.spec.js',
+	'/spec/tracks/26.3.trackIndications.spec.js',
+	'/spec/tracks/26.4.releaseInTrack.spec.js',
+	'/spec/tracks/26.5.createTrack.spec.js',
+	'/spec/tracks/26.6.trackPage.spec.js',
+	'/spec/tracks/26.7.approveReleaseInTrack.spec.js'
         ],
 suites: {		
 	      "00_logIn_suite": ['/login/login.spec.js'],
