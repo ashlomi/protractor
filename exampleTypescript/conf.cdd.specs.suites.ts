4ea1276884acf4455c3d089c7f0253f324cd3ee3
cde-test-user <cde.test.user@gmail.com>
2017-10-19 16:13:21 +0300

    Update conf.cdd.specs.suites.ts

--- exampleTypescript/conf.cdd.specs.suites.ts
+++ exampleTypescript/conf.cdd.specs.suites.ts
@@ -11,29 +11,29 @@ export let config: Config = {
 // Specs taken from CDD protractor tests
 // not to be executed here
 specs: [
-        '/releases/01.1.releasesPage.spec.ts',
-	'/releases/01.2.createRelease.spec.ts',
-        '/releases/01.3.releaseActions.spec.ts',
-	'/users/*.spec.ts',
-	'/tracks/26.1.tracksPage.spec.ts',
-	'/tracks/26.2.trackActions.spec.ts',
-	'/tracks/26.3.trackIndications.spec.ts',
-	'/tracks/26.4.releaseInTrack.spec.ts',
-	'/tracks/26.5.createTrack.spec.ts',
-	'/tracks/26.6.trackPage.spec.ts',
-	'/tracks/26.7.approveReleaseInTrack.spec.ts'
+        '/spec/releases/01.1.releasesPage.spec.ts',
+	'/spec/releases/01.2.createRelease.spec.ts',
+        '/spec/releases/01.3.releaseActions.spec.ts',
+	'/spec/users/*.spec.ts',
+	'/spec/tracks/26.1.tracksPage.spec.ts',
+	'/spec/tracks/26.2.trackActions.spec.ts',
+	'/spec/tracks/26.3.trackIndications.spec.ts',
+	'/spec/tracks/26.4.releaseInTrack.spec.ts',
+	'/spec/tracks/26.5.createTrack.spec.ts',
+	'/spec/tracks/26.6.trackPage.spec.ts',
+	'/spec/tracks/26.7.approveReleaseInTrack.spec.ts'
         ],
 	
 suites: {		
-	"00_logIn": ['/login/login.spec.ts'],
-        "00_logInSaas": '/login/loginSaas.spec.ts',
-        "01_releasesPageObject": ['/releases/01.1.releasesPage.spec.ts',
+	"00_logIn_suite": ['/login/login.spec.ts'],
+        "00_logInSaas_suite": '/login/loginSaas.spec.ts',
+        "01_releasesPageObject_suite": ['/releases/01.1.releasesPage.spec.ts',
                                   '/releases/01.2.createRelease.spec.ts',
                                   '/releases/01.3.releaseActions.spec.ts'],
-        "02_externalTask": '/tasks/externalTask.spec.ts',
-        "03_usersPageObjects": ['/users/*.users.spec.ts'],
-        "04_releasePageObjects": '/release/04release.spec.ts',
-        "05_mainWindowObjects": '/mainWindow/05mainWindowPO.spec.ts'
+        "02_externalTask_suite": '/tasks/externalTask.spec.ts',
+        "03_usersPageObjects_suite": ['/users/*.users.spec.ts'],
+        "04_releasePageObjects_suite": '/release/04release.spec.ts',
+        "05_mainWindowObjects_suite": '/mainWindow/05mainWindowPO.spec.ts'
 	},
 
   seleniumAddress: 'http://localhost:4444/wd/hub',
