5bccfc8f4816a8a6de06248fd888b566d17d574b
cde-test-user <cde.test.user@gmail.com>
2017-10-19 16:00:43 +0300

    Update conf.cdd.specs.suites.ts

--- exampleTypescript/conf.cdd.specs.suites.ts
+++ exampleTypescript/conf.cdd.specs.suites.ts
@@ -12,29 +12,30 @@ export let config: Config = {
 // not to be executed here
 specs: [
         '/releases/01.1.releasesPage.spec.ts',
-        '/releases/01.2.createRelease.spec.ts',
+	'/releases/01.2.createRelease.spec.ts',
         '/releases/01.3.releaseActions.spec.ts',
-        '/users/*.spec.ts',
-        '/tracks/26.1.tracksPage.spec.ts',
-        '/tracks/26.2.trackActions.spec.ts',
-        '/tracks/26.3.trackIndications.spec.ts',
-        '/tracks/26.4.releaseInTrack.spec.ts',
-        '/tracks/26.5.createTrack.spec.ts',
-        '/tracks/26.6.trackPage.spec.ts',
-        '/tracks/26.7.approveReleaseInTrack.spec.ts'
+	'/users/*.spec.ts',
+	'/tracks/26.1.tracksPage.spec.ts',
+	'/tracks/26.2.trackActions.spec.ts',
+	'/tracks/26.3.trackIndications.spec.ts',
+	'/tracks/26.4.releaseInTrack.spec.ts',
+	'/tracks/26.5.createTrack.spec.ts',
+	'/tracks/26.6.trackPage.spec.ts',
+	'/tracks/26.7.approveReleaseInTrack.spec.ts'
         ],
-suites: {		
-		    "00_logIn": ['/login/login.spec.ts'],
-        "00_logInSaas": '/login/loginSaas.spec.ts',
-        "01_releasesPageObject": ['/releases/01.1.releasesPage.spec.ts',
-                                  '/releases/01.2.createRelease.spec.ts',
-                                  '/releases/01.3.releaseActions.spec.ts'],
-        "02_externalTask": '/tasks/externalTask.spec.ts',
-        "03_usersPageObjects": ['/users/03.1.users.spec.ts',
-                                '/users/03.2.localUsers.spec.ts',
-                                '/users/03.3.remoteUsers.spec.ts'],
-        "04_releasePageObjects": '/release/04release.spec.ts',
-        "05_mainWindowObjects": '/mainWindow/05mainWindowPO.spec.ts'
+	
+suites: {
+	"00_logIn": ['/login/login.spec.ts'],
+	"00_logInSaas": '/login/loginSaas.spec.ts',
+	"01_releasesPageObject": ['/releases/01.1.releasesPage.spec.ts',
+				  '/releases/01.2.createRelease.spec.ts',
+				  '/releases/01.3.releaseActions.spec.ts'],
+	"02_externalTask": '/tasks/externalTask.spec.ts',
+	"03_usersPageObjects": ['/users/03.1.users.spec.ts',
+				'/users/03.2.localUsers.spec.ts',
+				'/users/03.3.remoteUsers.spec.ts'],
+	"04_releasePageObjects": '/release/04release.spec.ts',
+	"05_mainWindowObjects": '/mainWindow/05mainWindowPO.spec.ts'
 	},
 
   seleniumAddress: 'http://localhost:4444/wd/hub',
