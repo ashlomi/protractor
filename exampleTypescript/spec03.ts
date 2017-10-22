03f4f1b51bae7fd41968bb6f07e2af5e668ceff9
cde-test-user <cde.test.user@gmail.com>
2017-10-22 11:54:52 +0300

    Multiple spec files for CDD TAR wildcard import and execution

--- exampleTypescript/spec03.ts
+++ exampleTypescript/spec03.ts
@@ -1,4 +1,4 @@
-// Mutiple spec files for CDD TAR wildcard import and execution
+// Multiple spec files for CDD TAR wildcard import and execution
 import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
 
 describe('protractor with typescript typings', () => {
@@ -6,13 +6,13 @@ describe('protractor with typescript typings', () => {
     browser.get('http://www.angularjs.org');
   });
 
-  it('should greet the named user', () => {
+  it('should greet the named user 03', () => {
     element(by.model('yourName')).sendKeys('Julie');
     let greeting = element(by.binding('yourName'));
     expect(greeting.getText()).toEqual('Hello Julie!');
   });
 
-  it('should list todos', function() {
+  it('should list todos 04', function() {
     let todoList = element.all(by.repeater('todo in todoList.todos'));
     expect(todoList.count()).toEqual(2);
     expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
