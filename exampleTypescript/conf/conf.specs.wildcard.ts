d56f2cfb216834f54c042027fe162c3b7002b8d2
cde-test-user <cde.test.user@gmail.com>
2017-10-24 16:52:08 +0300

    Create conf.specs.wildcard.ts

---
+++ exampleTypescript/conf/conf.specs.wildcard.ts
@@ -0,0 +1,22 @@
+
+// For CDD TAR import and execute
+// Conf ts with specs array
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  
+specs: [
+        '../tar/spec.ts',
+        '../tar/spec0*.ts'
+        ],
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
