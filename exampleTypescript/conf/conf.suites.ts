1213b007dde298965cf553f891a3017a8d00c1f5
cde-test-user <cde.test.user@gmail.com>
2017-10-24 17:10:33 +0300

    Create conf.suites.ts

---
+++ exampleTypescript/conf/conf.suites.ts
@@ -0,0 +1,25 @@
+// CDD TAR
+// conf suites file
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  
+suites: {		
+		"00_logIn": ['/login/login.spec.ts'],
+        "Suite00": '../tar/spec.ts',
+        "Suite01": '../tar/spec01.ts',
+        "Suite02": '../tar/spec02.ts',
+        "Suite03": '../tar/spec03.ts',
+        "Suite04": '../tar/spec04.ts'
+	},
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
