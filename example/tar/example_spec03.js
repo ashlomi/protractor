b16daca0a010023df2e1be23f1b0167931df1859
cde-test-user <cde.test.user@gmail.com>
2017-10-24 08:57:37 +0300

    spec.js file for CDD TAR 03

---
+++ example/tar/example_spec03.js
@@ -0,0 +1,38 @@
+// spec.js file for CDD TAR 03
+describe('angularjs homepage 03', function() {
+  it('should greet the named user 03', function() {
+    browser.get('http://www.angularjs.org');
+
+    element(by.model('yourName')).sendKeys('Julie');
+
+    var greeting = element(by.binding('yourName'));
+
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  describe('todo list 03', function() {
+    var todoList;
+
+    beforeEach(function() {
+      browser.get('http://www.angularjs.org');
+
+      todoList = element.all(by.repeater('todo in todoList.todos'));
+    });
+
+    it('should list todos 03', function() {
+      expect(todoList.count()).toEqual(2);
+      expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+    });
+
+    it('should add a todo 03', function() {
+      var addTodo = element(by.model('todoList.todoText'));
+      var addButton = element(by.css('[value="add"]'));
+
+      addTodo.sendKeys('write a protractor test');
+      addButton.click();
+
+      expect(todoList.count()).toEqual(3);
+      expect(todoList.get(2).getText()).toEqual('write a protractor test');
+    });
+  });
+});
