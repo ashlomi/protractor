88a781426e8d4be422ac8b1cb810859bf2b35579
cde-test-user <cde.test.user@gmail.com>
2017-10-26 12:17:27 +0300

    specs js

---
+++ example/conf/conf.specs.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: [
+        '../tar/example_spec01.js',
+        '../tar/example_spec02.js',
+        '../tar/example_spec03.js',
+        '../tar/example_spec04.js'
+        ],
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
