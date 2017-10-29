0de345808c12cabe4d2c74775a09f161bad1d933
cde-test-user <cde.test.user@gmail.com>
2017-10-29 18:38:08 +0200

    Update spec01.mix.small.ts

--- exampleTypescript/tar/spec01.mix.small.ts
+++ exampleTypescript/tar/spec01.mix.small.ts
@@ -2,25 +2,7 @@
 // Multiple spec files for CDD TAR wildcard import and execution
 import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
 
-describe('CDD TAR all TC PASS 01 A', () => {
-  beforeEach(() => {
-    browser.get('http://www.angularjs.org');
-  });
-
-  it('should greet the named user 01 A', () => {
-    element(by.model('yourName')).sendKeys('Julie');
-    let greeting = element(by.binding('yourName'));
-    expect(greeting.getText()).toEqual('Hello Julie!');
-  });
-
-  it('should list todos 01 A', function() {
-    let todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(2);
-    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-  });
-});
-
-// describe('CDD TAR all tc fail 01 A', () => {
+// describe('CDD TAR all TC PASS 01 A', () => {
 //   beforeEach(() => {
 //     browser.get('http://www.angularjs.org');
 //   });
@@ -28,12 +10,30 @@ describe('CDD TAR all TC PASS 01 A', () => {
 //   it('should greet the named user 01 A', () => {
 //     element(by.model('yourName')).sendKeys('Julie');
 //     let greeting = element(by.binding('yourName'));
-//     expect(greeting.getText()).toEqual('FAIL A!');
+//     expect(greeting.getText()).toEqual('Hello Julie!');
 //   });
 
 //   it('should list todos 01 A', function() {
 //     let todoList = element.all(by.repeater('todo in todoList.todos'));
 //     expect(todoList.count()).toEqual(2);
-//     expect(todoList.get(1).getText()).toEqual('FAIL A');
+//     expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
 //   });
 // });
+
+describe('CDD TAR all tc fail 01 A', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01 A', () => {
+    element(by.model('yourName')).sendKeys('Julie');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('FAIL A!');
+  });
+
+  it('should list todos 01 A', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('FAIL A');
+  });
+});
