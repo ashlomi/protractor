aff8ddaedd624457baedd8a754f2853e1b689c70
cde-test-user <cde.test.user@gmail.com>
2017-10-29 12:38:45 +0200

    conf suites file - suites with wildcards

---
+++ exampleTypescript/conf/conf.suites.wildcard.ts
@@ -0,0 +1,21 @@
+// CDD TAR
+// conf suites file - suites with wildcards
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  
+suites: {
+        "specs_pass": ['../tar/spec0*.ts'],
+        "specs_fail": ['../tar/fail0*.ts']
+	},
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
