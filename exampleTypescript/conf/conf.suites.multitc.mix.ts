e704aa7623fe9d244e11acbb51b14bf94f888dad
cde-test-user <cde.test.user@gmail.com>
2017-10-29 16:56:49 +0200

    Create conf.suites.multitc.mix.ts

---
+++ exampleTypescript/conf/conf.suites.multitc.mix.ts
@@ -0,0 +1,21 @@
+// CDD TAR
+// conf suites file with all ts specs
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  
+suites: {
+        "spec01.mix.ts": '../tar/spec01.mix.ts',
+	      "spec02.mix.ts": '../tar/spec01.mix.ts'
+	},
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
