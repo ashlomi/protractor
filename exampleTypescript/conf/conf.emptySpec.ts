7565b6f3cd971e72dc852aa0fc7414413a89e93e
cde-test-user <cde.test.user@gmail.com>
2017-11-07 17:15:05 +0200

    conf with a spec that has no test cases inside

---
+++ exampleTypescript/conf/conf.emptySpec.ts
@@ -0,0 +1,20 @@
+// CDD TAR
+// conf suites file with all ts specs
+import {Config} from 'protractor';
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  
+suites: {
+        "spec.empty.ts": '../tar/spec.empty.ts'
+	},
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // You could set no globals to true to avoid jQuery '$' and protractor '$'
+  // collisions on the global namespace.
+  noGlobals: true
+};
