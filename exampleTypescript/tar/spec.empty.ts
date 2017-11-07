68e051c82935351dd0c422ab414ba9ac24adc2a6
cde-test-user <cde.test.user@gmail.com>
2017-11-07 17:11:59 +0200

    Update spec.empty.ts

--- exampleTypescript/tar/spec.empty.ts
+++ exampleTypescript/tar/spec.empty.ts
@@ -2,19 +2,19 @@
 import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
 
 describe('protractor with typescript typings 01', () => {
-  beforeEach(() => {
-    browser.get('http://www.angularjs.org');
-  });
+//   beforeEach(() => {
+//     browser.get('http://www.angularjs.org');
+//   });
 
-  xit('should greet the named user 01', () => {
-    element(by.model('yourName')).sendKeys('Julie');
-    let greeting = element(by.binding('yourName'));
-    expect(greeting.getText()).toEqual('Hello Julie!');
-  });
+//   it('should greet the named user 01', () => {
+//     element(by.model('yourName')).sendKeys('Julie');
+//     let greeting = element(by.binding('yourName'));
+//     expect(greeting.getText()).toEqual('Hello Julie!');
+//   });
 
-  xit('should list todos 01', function() {
-    let todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(2);
-    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-  });
+//   it('should list todos 01', function() {
+//     let todoList = element.all(by.repeater('todo in todoList.todos'));
+//     expect(todoList.count()).toEqual(2);
+//     expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+//   });
 });
