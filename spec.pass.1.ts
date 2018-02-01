179faefa2441efb3c6303d8b43c21bdcb7a9868a
cde-test-user <cde.test.user@gmail.com>
2018-02-01 09:21:21 +0200

    Create spec.pass.1.ts

---
+++ spec.pass.1.ts
@@ -0,0 +1,19 @@
+import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
+
+describe('Two test cases that pass 01', () => {
+  beforeEach(() => {
+    browser.get('http://www.angularjs.org');
+  });
+
+  it('should greet the named user 01', () => {
+    element(by.model('yourName')).sendKeys('Yonatan');
+    let greeting = element(by.binding('yourName'));
+    expect(greeting.getText()).toEqual('Hello Yonatan!');
+  });
+
+  it('should list todos 01', function() {
+    let todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+  });
+});
