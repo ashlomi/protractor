8524462bea3823327bebafdfbe2746d1d33478e9
cde-test-user <cde.test.user@gmail.com>
2018-02-01 13:28:37 +0200

    Update spec.pass.2.ts

--- exampleTypescript/tar/spec.pass.2.ts
+++ exampleTypescript/tar/spec.pass.2.ts
@@ -6,13 +6,13 @@ describe('Two test cases that pass 02', () => {
     browser.get('http://www.angularjs.org');
   });
 
-  it('should greet the named user 02', () => {
+  it('should greet the named user 02 - Pass', () => {
     element(by.model('yourName')).sendKeys('Yonatan');
     let greeting = element(by.binding('yourName'));
     expect(greeting.getText()).toEqual('Hello Yonatan!');
   });
 
-  it('should list todos 02', function() {
+  it('should list todos 02 - Pass', function() {
     let todoList = element.all(by.repeater('todo in todoList.todos'));
     expect(todoList.count()).toEqual(2);
     expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
